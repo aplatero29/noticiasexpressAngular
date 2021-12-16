4544c31d001227b1ec00f4bdd08438071c1b51ab
aplatero29 <alejandroplatero29@gmail.com>
2021-12-16 19:13:02 +0100

    CSS testing

--- src/app/vistas/entradas/homepage-entrada/homepage-entrada.component.ts
+++ src/app/vistas/entradas/homepage-entrada/homepage-entrada.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../../servicios/api/api.service';
 import { Router } from '@angular/router';
 import { EntradasI } from 'src/app/modelos/listarentradas.interface';
-import { NgxPaginationModule } from "ngx-pagination";
-import { CategoriasI } from 'src/app/modelos/listarcategorias.interface';
 
 @Component({
   selector: 'app-homepage-entrada',
@@ -13,10 +11,8 @@ import { CategoriasI } from 'src/app/modelos/listarcategorias.interface';
 export class HomepageEntradaComponent implements OnInit {
   entradas: EntradasI[] = [];
   page: number = 1;
-  pageSize: number = 6;
-  /* itemsPorPagina: number = 5 */
-  /* paginacion: any = []; */
-  /*   categorias: CategoriasI[] = []; */
+  pageSize: number = 10;
+  
 
   constructor(private api: ApiService, private router: Router) {}
 
@@ -24,20 +20,14 @@ export class HomepageEntradaComponent implements OnInit {
     this.api.getAllEntradas(this.page, this.pageSize).subscribe((data) => {
       /***************  NO TOCAR -----  TRANSFORMA EL JSON EN UN ARRAY, Y NUEVAMENTE OTRO ARRAY */
       this.entradas = Object.values(data);
-      /* this.paginacion = Object.values(this.entradas)
-      
-      console.log(this.paginacion[2]) */
+
       console.log(this.entradas);
       console.log(this.entradas[0]);
       this.entradas = Object.values(this.entradas[0]);
       /************************ */
       this.parseFechas();
+      console.log(this.entradas);
     });
-
-    /*     this.api.getAllCategorias().subscribe((data) => {
-      this.categorias = Object.values(data);
-      this.categorias = Object.values(this.categorias[0]);
-    }); */
   }
 
   parseFechas() {
@@ -52,8 +42,6 @@ export class HomepageEntradaComponent implements OnInit {
     console.log(id);
     this.router.navigate(['entrada', id]);
   }
-
-  
 
   /*   verEntradasEnCategoria(id: number) {
     console.log(id);
